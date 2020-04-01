114e9957cf79c5a8cd176f4e5806d396a4a725c0
Adler <adlermpf@me.com>
2020-04-01 14:53:13 -0300

    enviando arquivos

---
+++ controllers/homeController.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const fs = require('fs');
+const homeController = {
+  index: (req, res) => {
+    let servicos = [
+      { nome: 'Dev Full Stack', imagem: '/imagens/undraw_dev_focus.svg'},
+      { nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
+      { nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'}
+    ];
+
+    let banners = [
+      '/imagens/banner_ux.jpg', 
+      '/imagens/full_stack_banner.jpg', 
+      '/imagens/banner.jpg'
+    ];
+
+    res.render(
+      'index', 
+      { title: 'Home', listaServicos: servicos, listaBanners: banners }
+    );
+  },
+  contato: (req, res) => {
+    let {nome, email, mensagem} = req.body;
+    let datetime = new Date().getTime();
+    
+
+   let infoContato = {nome, email,  mensagem, datetime};
+    
+    const fileContatos = path.join('db', "contatos.json");
+
+    let listaContato = {};
+
+    if(fs.existsSync(fileContatos)){
+      
+      listaContato = JSON.parse(fs.readFileSync(fileContatos, {encoding: 'utf-8'}));
+      
+      listaContato.push(infoContato);
+    } else {
+      listaContato = [{
+        nome, email, mensagem, datetime
+      }];
+    }
+    listaContato = JSON.stringify(listaContato);
+    
+    fs.writeFileSync(fileContatos, listaContato);
+
+    res.render('contato', {nome, email, mensagem, title: 'Contato'});
+  },
+
+  newsletter: (req, res) => {
+    let {emailnewsletter} = req.body;
+
+    const fileNewsLetter = path.join('db', 'newsletter.json');
+
+    let listaNewsLetter = {};
+
+    if(fs.existsSync(fileNewsLetter)){
+      //trazendo conteudo do arquivo em formato JSON
+      listaNewsLetter = fs.readFileSync(fileNewsLetter, {encoding: 'utf-8'});
+      //transformando JSON em obj
+      listaNewsLetter = JSON.parse(listaNewsLetter);
+      // pegando array de inscritos e adicionando um novo email;
+      listaNewsLetter.inscritos.push(emailnewsletter);
+    } else {
+      listaNewsLetter = {
+        inscritos: [emailnewsletter]
+      };
+      
+    }
+    // transforma em obj em JSON
+    listaNewsLetter = JSON.stringify(listaNewsLetter);
+    // guardando a lsita de inscritos com o novo email
+    fs.writeFileSync(fileNewsLetter, listaNewsLetter);
+
+    res.render('newsletter', {emailnewsletter, title: "Newsletter"});
+  }
+};
+
+module.exports = homeController;
